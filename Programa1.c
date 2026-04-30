e1e00044869c9c98f872093df3db9617e4b8e116
santi-estilla20 <os967577@gmail.com>
2026-04-30 13:46:46 -0600

    Update Programa1.c

--- Programa1.c
+++ Programa1.c
@@ -7,12 +7,19 @@ int main()
 {
  char nombre[] = "Facultad de Ingenieria";
  imprimir(nombre);
-return 0
+return 0;
 }
 // Implementación de las funciones del programa
-void imprimir(char s[]){
+void imprimir(char s[])
+{
  int tam;
+ tam=strlen(s);
+  printf("El tamaño del string es %d\n", tam);
+ 
  for ( tam=strlen(s)-1 ; tam>=0 ; tam--)
+  {
+   printf("El tamaño del string es: %d\n", tam);
    printf("%c", s[tam]);
  printf("\n");
+  }
 }
