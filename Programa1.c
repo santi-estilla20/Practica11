0b6a5da0b52533602e3295d11fe1decf550dfcf3
santi-estilla20 <os967577@gmail.com>
2026-04-30 13:38:13 -0600

    Create Programa1.c

---
+++ Programa1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+// Prototipo o firma de las funciones del programa
+void imprimir(char[]);
+// Definición o implementación de la función main
+int main()
+{
+ char nombre[] = "Facultad de Ingenieria";
+ imprimir(nombre);
+return 0
+}
+// Implementación de las funciones del programa
+void imprimir(char s[]){
+ int tam;
+ for ( tam=strlen(s)-1 ; tam>=0 ; tam--)
+   printf("%c", s[tam]);
+ printf("\n");
+}
